e74a580a3f012e41170504b71ae170a1ddcb32c4
Kevin Hanley <kvnhanley@gmail.com>
2019-09-02 23:10:45 +0100

    testing media

--- home.php
+++ home.php
@@ -6,6 +6,9 @@
 	<link rel="stylesheet" href="./Navigation/navbar.css" type="text/css"/>
   <link rel="stylesheet" href="./home.css" type="text/css"/>
 
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<!-- JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   
