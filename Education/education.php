c9598ad3e85e7a45005b5cb5ff22d15b5d0cbe42
Kevin Hanley <kvnhanley@gmail.com>
2019-09-02 16:49:23 +0100

    filled eucation

--- Education/education.php
+++ Education/education.php
@@ -2,9 +2,43 @@
 <html>
 <head>
 	<title>Education</title>
+
+	<link rel="stylesheet" href="../../Navigation/navbar.css" type="text/css"/>
+  	<link rel="stylesheet" href="./education.css" type="text/css"/>
+
+	<!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  
+    <script>
+      $(document).ready(function(){          
+          $('.logo').on('click', function(){
+              $(location).attr('href', '/home.php');
+          }); 
+      });
+    </script>
+
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400');
+    </style>
 </head>
+
+
 <body>
-	<p>hello></p>
+	<!-- this php will add the navbar -->
+	<?php include("../../Navigation/navigation.html"); ?>
+
+	<div id="introduction">
+
+    <div class="text-box">
+      <h1>This is the education section!</h1>
+
+      
+    </div>
+     
+  </div>
 
 </body>
 </html>
