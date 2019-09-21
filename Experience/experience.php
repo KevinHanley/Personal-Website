e6b5042dd2a655278a1a1b9d9a96e4e28128fc29
Kevin Hanley <kvnhanley@gmail.com>
2019-09-21 13:45:38 +0100

    added css

--- Experience/experience.php
+++ Experience/experience.php
@@ -35,54 +35,59 @@
 	<!-- This is the content displayed on the page -->
 	<div id="content">
 
-		<div class="toss-1">
-	      <h1>Toss Bryans Ltd.</h1>
+		<div class="mini-con">
+			<div class="toss-1">
+		      <h1>Toss Bryans Ltd.</h1>
 
-	      <h3>2015-2019</h3>
+		      <h3>2015-2019</h3>
+		    </div>
+
+
+			<div class="toss-2">
+		      <h3>Summary</h3>
+
+		      <p><b>Role:</b> Sales Assisstant</p>
+
+		      <p>During my time in Toss Bryans I worked in several different departments, ranging from DIY to Domestic Appliances to Tills. My core tasks involved; helping customers with in-store purchases, dealing with customer complaints, ordering and maintaing stock levels, handling money and training new part-time staff.</p>
+		    </div>
 	    </div>
 
 
-		<div class="toss-2">
-	      <h3>Summary</h3>
 
-	      <p><b>Role:</b> Sales Assisstant</p>
+	    <div class="mini-con">
+	    	<div class="cd-1">
+		      <h1>CoderDojo</h1>
 
-	      <p>During my time in Toss Bryans I worked in several different departments, ranging from DIY to Domestic Appliances to Tills. My core tasks involved; helping customers with in-store purchases, dealing with customer complaints, ordering and maintaing stock levels, handling money and training new part-time staff.</p>
-	    </div>
-
-
-
-    	<div class="cd-1">
-	      <h1>CoderDojo</h1>
-
-	      <h3>2018</h3>
-	    </div>
+		      <h3>2018</h3>
+		    </div>
 
 
-		<div class="cd-2">
-	      <h3>Summary</h3>
+			<div class="cd-2">
+		      <h3>Summary</h3>
 
-	      <p><b>Role:</b> Coding Instructor</p>
+		      <p><b>Role:</b> Coding Instructor</p>
 
-	      <p>The CoderDojo program was run in UCC over a number of weeks in late 2018. Its core goal is to teach children the fundamental principles of computer coding, in a fun and interactive way. A number of students, including myself, helped facilitate this learning process by engaging with the online source material and working as a cohesive team.</p>
-	    </div>
-
-
-	    <div class="bg-test">
-			    <div class="camp-1">
-			      <h1>Camp Sloane YMCA</h1>
-
-			      <h3>2019</h3>
-			    </div>
+		      <p>The CoderDojo program was run in UCC over a number of weeks in late 2018. Its core goal is to teach children the fundamental principles of computer coding, in a fun and interactive way. A number of students, including myself, helped facilitate this learning process by engaging with the online source material and working as a cohesive team.</p>
+		    </div>
+		</div>
 
 
-				<div class="camp-2">
-			      <h3>Summary</h3>
 
-			      <p><b>Role:</b> Camp Counsellor</p>
+	    <div class="mini-con">
+		    <div class="camp-1">
+		      <h1>Camp Sloane YMCA</h1>
 
-			      <p>In the summer of 2019 I travelled to Conneticut in the USA, where I worked as a camp counsellor in Camp Sloane YMCA. Sending kids to summer camps is a big American tradition, and one I was very lucky to be apart of. Here, I became part of a team of young highly-motivated individuals who had come from around the world. Like myself, they had not only come to grow and develop individually, but also to have an unforgettable experience and to make many friends along the way.</p>
-			    </div>
+		      <h3>2019</h3>
+		    </div>
+
+
+			<div class="camp-2">
+		      <h3>Summary</h3>
+
+		      <p><b>Role:</b> Camp Counsellor</p>
+
+		      <p>In the summer of 2019 I travelled to Conneticut in the USA, where I worked as a camp counsellor in Camp Sloane YMCA. Sending kids to summer camps is a big American tradition, and one I was very lucky to be apart of. Here, I became part of a team of young highly-motivated individuals who had come from around the world. Like myself, they had not only come to grow and develop individually, but also to have an unforgettable experience and to make many friends along the way.</p>
+		    </div>
 	    </div>
 	    
 	</div>
