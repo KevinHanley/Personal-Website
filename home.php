30378a14ca4c12bbb7bec17ebf963e2731a3710e
Kevin Hanley <kvnhanley@gmail.com>
2019-09-22 10:46:16 +0100

    icon changes

--- home.php
+++ home.php
@@ -13,13 +13,22 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
   <script>
-    $(document).ready(function(){          
+    $(document).ready(function(){   
+
+      /* clicking name returns to homepage */       
         $('.logo').on('click', function(){
             $(location).attr('href', './home.php');
         }); 
 
+        /* clicking menu icon opens menu and toggles icon */
         $('i').on('click', function(){
             $('#mobile-links').toggle();
+
+            if($(this).text() == 'menu'){
+              $(this).text('clear');
+            }else{
+              $(this).text('menu');
+            }
         }); 
 
     });
